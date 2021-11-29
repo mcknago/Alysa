d20d62281aade5efcbdc4d8eae631757cff8e368
mcknago <74916664+mcknago@users.noreply.github.com>
2021-11-28 23:08:51 -0500

    Delete conexion.php

--- conexion.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$host = "alysa.cm8obvyhkvbx.us-east-2.rds.amazonaws.com";  #Si es en otro lugar va el HTTP..
-$username = "admin";
-$password = "finaltelematica";
-$registro = "Alysa";
-$connection = mysqli_connect($host, $username, $password, $registro);
-?>
