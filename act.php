2b76f4f2b998d7c66184611588e786cca98e31a4
Mateo Fernández <89306590+mateostiven@users.noreply.github.com>
2021-11-29 21:34:24 -0500

    Add files via upload

---
+++ act.php
@@ -0,0 +1,16 @@
+<?php 
+    include 'config.php';
+    if(isset($_POST['new_estado'], $_POST['date'])){
+    $conexion = new mysqli($Host, $Usuario, $Clave, 'Alysa');
+    $new_estado=$_POST['new_estado'];
+    $date=$_POST[''];
+
+    $sql = "SELECT * FROM casos, estados WHERE id = $dato";
+    $result = $conexion->query($sql);
+
+
+    $sql = "INSERT INTO `estados` (estado,fecha)
+    VALUES('$new_estado','$date')";
+    $conexion->query($sql);
+
+?>
